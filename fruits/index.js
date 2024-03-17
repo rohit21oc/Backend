548a3e98b600f9fad312f8330be0224d7d08c2a4
ROHIT KUMAR <119073894+rohit21oc@users.noreply.github.com>
2024-03-17 21:32:37 +0530

    Add files via upload

---
+++ fruits/index.js
@@ -0,0 +1,7 @@
+const apple = require("./apple")
+const banana = require("./banana")
+const mango = require("./mango")
+const orange = require("./orange")
+
+let fruits = [apple,banana,mango,orange];
+module.exports = fruits;
